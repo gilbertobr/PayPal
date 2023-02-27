72bedf1ec79589be1d8196a433480410ea0fd1de
Gilberto <gp_sj@hotmail.com>
2023-02-26 22:25:48 -0300

    Update Version Httpoison

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule PayPal.Mixfile do
 
   defp deps do
     [
-      {:httpoison, "~> 0.13"},
+      {:httpoison, "~> 2.0"},
       {:poison, "~> 5.0"},
       {:oauth2, "~> 0.9"},
       {:exvcr, "~> 0.8", only: [:dev, :test]},
